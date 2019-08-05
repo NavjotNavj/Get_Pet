7ff21aa89f4fd2f9f7babfc22ba3dd00c5142ff4
NavjotNavj <navjots8867@gmail.com>
2019-08-06 04:44:56 +0530

    UI changes

--- app/src/main/java/com/example/myapplication/Moldels/Home.java
+++ app/src/main/java/com/example/myapplication/Moldels/Home.java
@@ -3,22 +3,27 @@ package com.example.myapplication.Moldels;
 import android.app.Application;
 import android.content.Intent;
 
-import com.example.myapplication.listing_animals;
+import com.example.myapplication.SplashActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Home extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
 
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser  = firebaseAuth.getCurrentUser();
+//        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+//        FirebaseUser firebaseUser  = firebaseAuth.getCurrentUser();
+//
+//        if(firebaseUser != null ){
+//            Intent myIntent = new Intent(Home.this, SplashActivity.class);
+//            myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivity(myIntent);
+//        }
 
-        if(firebaseUser != null ){
-            Intent myIntent = new Intent(Home.this, listing_animals.class);
-            myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(myIntent);
-        }
+
+
+
     }
 }
